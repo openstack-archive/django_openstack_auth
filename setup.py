985bf5659cddec4e60d33f0e1c709e0647fab4ca
Monty Taylor <mordred@inaugust.com>
2013-09-10 16:57:19 -0500

    Align with OpenStack project standards
    Change-Id: If03b325c941dcac257b25b90c8b152dcead5edd2

--- setup.py
+++ setup.py
@@ -1,52 +1,22 @@
-import os
-import re
-import codecs
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import setuptools
 
-def read(*parts):
-    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
-setup(
-    name="django_openstack_auth",
-    version=find_version("openstack_auth", "__init__.py"),
-    url='http://django_openstack_auth.readthedocs.org/',
-    license='BSD',
-    description=("A Django authentication backend for use with the "
-                 "OpenStack Keystone Identity backend."),
-    long_description=read('README.rst'),
-    author='Gabriel Hurley',
-    author_email='gabriel@strikeawe.com',
-    packages=find_packages(),
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP',
-    ],
-    zip_safe=False,
-    install_requires=[
-        'django >= 1.4',
-        'python-keystoneclient >= 0.3'
-    ],
-    tests_require=[
-        'mox',
-    ],
-    test_suite='openstack_auth.tests.run_tests.run'
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
